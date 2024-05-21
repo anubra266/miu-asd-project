4283612da75c34ef836bb851733a12ced6385fe7
Reza Un Nabi <reza.newgen@gmail.com>
2024-05-21 12:36:54 -0500

    deposit,withdraw

--- app/framework/Account.java
+++ app/framework/Account.java
@@ -1,11 +1,44 @@
 package app.framework;
 
-public abstract class  Account {
+import app.ui.bank.AccountEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public  class  Account {
     public String AccountNumber;
     public double balance;
 
-    public abstract void deposite(String accountNumber,double amount,String message);
-    public abstract  void withdraw(String accountNumber,double amount,String message);
-    public abstract void addInterest(String accountNumber,double amount,String message);
+    private List<AccountEntry> entryList = new ArrayList<AccountEntry>();
 
+
+    public String getAccountNumber() {
+        return AccountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        AccountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    private void addEntry(AccountEntry entry) {
+        entryList.add(entry);
+    }
+    
+    public void deposit(double amount) {
+        AccountEntry entry = new AccountEntry(amount, "deposit", "", "");
+        entryList.add(entry);
+    }
+
+    public void withdraw(double amount) {
+        AccountEntry entry = new AccountEntry(-amount, "withdraw", "", "");
+        entryList.add(entry);
+    }
 }
