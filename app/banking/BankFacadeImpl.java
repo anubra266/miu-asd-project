6e38a374e2d051c06ef20e60335ad0f482160669
Reza Un Nabi <reza.newgen@gmail.com>
2024-05-22 12:19:03 -0500

    check account number uniqe, check null account

--- app/banking/BankFacadeImpl.java
+++ app/banking/BankFacadeImpl.java
@@ -5,7 +5,7 @@ import app.banking.customer.Organization;
 import app.banking.strategies.CheckingPercentageStrategy;
 import app.banking.strategies.SavingPercentageStrategy;
 import app.framework.*;
-import app.framework.persistence.DAO;
+
 
 import java.time.LocalDate;
 
@@ -25,53 +25,63 @@ public class BankFacadeImpl extends Subject implements BankFacade{
 
     @Override
     public void createPersonalAccount(String accNr, String name, String street, String city, String state, String zip, LocalDate birthDate, String email,String accountType) {
-        if(accountType.equals("checking")){
-            percentageStrategy = new CheckingPercentageStrategy();
-        }else{
-            percentageStrategy = new SavingPercentageStrategy();
-        }
+        if (bankAccountDatabase.isUnique(accNr)) {
+            if (accountType.equals("checking")) {
+                percentageStrategy = new CheckingPercentageStrategy();
+            } else {
+                percentageStrategy = new SavingPercentageStrategy();
+            }
 
-        Address address = new Address(street,state,city,zip);
-        Customer customer = new Individual(name,email,address,birthDate);
-        BankAccount account = new BankAccount(accNr,customer);
-        account.setPercentageStrategy(percentageStrategy);
-        bankAccountDatabase.save(accNr,account);
+            Address address = new Address(street, state, city, zip);
+            Customer customer = new Individual(name, email, address, birthDate);
+            BankAccount account = new BankAccount(accNr, customer);
+            account.setPercentageStrategy(percentageStrategy);
+            bankAccountDatabase.save(accNr, account);
+        }
     }
 
     @Override
     public void createCompanyAccount(String accNr, String name, String street, String city, String state, String zip, int numberOfEmployee, String email,String accountType) {
-       if(accountType.equals("checking")){
-             percentageStrategy = new CheckingPercentageStrategy();
-       }else{
-            percentageStrategy = new SavingPercentageStrategy();
-       }
+        if (accountType.equals("checking")) {
+            if (accountType.equals("checking")) {
+                percentageStrategy = new CheckingPercentageStrategy();
+            } else {
+                percentageStrategy = new SavingPercentageStrategy();
+            }
 
-        Address address = new Address(street,state,city,zip);
-        Customer customer = new Organization(name,email,address,numberOfEmployee);
-        BankAccount account = new BankAccount(accNr,customer);
-        account.setPercentageStrategy(percentageStrategy);
-        bankAccountDatabase.save(accNr,account);
+            Address address = new Address(street, state, city, zip);
+            Customer customer = new Organization(name, email, address, numberOfEmployee);
+            BankAccount account = new BankAccount(accNr, customer);
+            account.setPercentageStrategy(percentageStrategy);
+            bankAccountDatabase.save(accNr, account);
+        }
     }
 
     @Override
     public void withDraw(String accNumber,double amount) {
         BankAccount bankAccount = bankAccountDatabase.get(accNumber);
-        bankAccount.withdraw(amount,"Amount withdraw");
-        bankAccountDatabase.update(bankAccount.getAccNumber(),bankAccount);
+        if(bankAccount != null) {
+            bankAccount.withdraw(amount, "Amount withdraw");
+            bankAccountDatabase.update(bankAccount.getAccNumber(), bankAccount);
+        }
     }
 
     @Override
     public void deposit(String accNumber,double amount) {
         BankAccount bankAccount = bankAccountDatabase.get(accNumber);
-        bankAccount.deposit(amount,"Amount deposit");
-        bankAccountDatabase.update(bankAccount.getAccNumber(),bankAccount);
+        if(bankAccount != null) {
+            bankAccount.deposit(amount, "Amount deposit");
+            bankAccountDatabase.update(bankAccount.getAccNumber(), bankAccount);
+        }
     }
 
     @Override
     public void addInterest(String accNumber) {
         BankAccount bankAccount = bankAccountDatabase.get(accNumber);
-        bankAccount.addInterest();
-        bankAccountDatabase.update(bankAccount.getAccNumber(),bankAccount);
+        if(bankAccount != null) {
+            bankAccount.addInterest();
+            bankAccountDatabase.update(bankAccount.getAccNumber(), bankAccount);
+        }
     }
 
     @Override
