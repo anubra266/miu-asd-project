71fe20c98fcf2afa1b15a46db417a82a562d2921
roshan <mailme.roshanbhattarai@gmail.com>
2024-05-21 21:50:58 -0500

    chore: add subject class

---
+++ app/framework/Subject.java
@@ -0,0 +1,24 @@
+package app.framework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Subject implements Observable{
+    private List<Observer> observerList = new ArrayList<>();
+
+    @Override
+    public void register(Observer ob) {
+        this.observerList.add(ob);
+    }
+
+    @Override
+    public void unregister(Observer ob) {
+        this.observerList.remove(ob);
+    }
+
+    public List<Observer> getObserverList() {
+        return this.observerList;
+    }
+
+    public abstract void alert();
+}
