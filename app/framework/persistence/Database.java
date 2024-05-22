ad3e704614b08a0eacbbfd28982c6d3032387b41
roshan <mailme.roshanbhattarai@gmail.com>
2024-05-22 12:01:02 -0500

    chore: unique check constraint added

--- app/framework/persistence/Database.java
+++ app/framework/persistence/Database.java
@@ -3,6 +3,7 @@ package app.framework.persistence;
 import java.util.Collection;
 
 public interface Database<T> {
+    boolean isUnique(String id);
     void save(String id, T data);
     void update(String id, T data);
     T get(String id);
