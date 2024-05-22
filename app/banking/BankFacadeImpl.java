865bed147544d26bc10430eacbca70b8c1df0d95
roshan <mailme.roshanbhattarai@gmail.com>
2024-05-21 21:50:49 -0500

    chore: convert facade to singleton

--- app/banking/BankFacadeImpl.java
+++ app/banking/BankFacadeImpl.java
@@ -1,8 +1,20 @@
 package app.banking;
 
+import app.framework.Observer;
+import app.framework.Subject;
+
 import java.time.LocalDate;
 
-public class BankFacadeImpl implements BankFacade{
+public class BankFacadeImpl extends Subject implements BankFacade{
+
+    private static BankFacadeImpl instance = new BankFacadeImpl();
+
+    private BankFacadeImpl(){};
+
+    public static BankFacadeImpl getInstance(){
+        return BankFacadeImpl.instance;
+    }
+
     @Override
     public void createPersonalAccount(String accNr, String name, String street, String city, String state, String zip, LocalDate birthDate, String email) {
 
@@ -26,5 +38,12 @@ public class BankFacadeImpl implements BankFacade{
     @Override
     public void addInterest() {
 
+    }
+
+    @Override
+    public void alert() {
+        for(Observer o: this.getObserverList()){
+            o.callback(null);
+        }
     }
 }
--- app/creditcard/CreditCardFacadeImpl.java
+++ app/creditcard/CreditCardFacadeImpl.java
@@ -1,8 +1,20 @@
 package app.creditcard;
 
+import app.framework.Observer;
+import app.framework.Subject;
+
 import java.time.LocalDate;
 
-public class CreditCardFacadeImpl implements CreditCardFacade{
+public class CreditCardFacadeImpl extends Subject implements CreditCardFacade{
+
+    private static CreditCardFacadeImpl instance = new CreditCardFacadeImpl();
+
+    private CreditCardFacadeImpl(){};
+
+    public static CreditCardFacadeImpl getInstance(){
+        return CreditCardFacadeImpl.instance;
+    }
+
     @Override
     public void createAccount(String name, String street, String city, String state, String zip, String Email, String ccNumber, LocalDate exprDate) {
 
@@ -21,5 +33,12 @@ public class CreditCardFacadeImpl implements CreditCardFacade{
     @Override
     public void deposit(double amount) {
 
+    }
+
+    @Override
+    public void alert() {
+        for(Observer o: this.getObserverList()){
+            o.callback(null);
+        }
     }
 }
